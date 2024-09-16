3026671dda515412199626bb0f47f3ac15a66c48
Sebastián Sánchez Bedoya <sesabe10@gmail.com>
2024-09-16 09:02:23 -0500

    Subiendo

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         Conversion conversion = new Conversion();
         conversion.iniciar();
     }
